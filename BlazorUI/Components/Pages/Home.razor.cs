d435d26c092d4921df77ae531466d12173d98f48
Ali Ilci <Ali.Ilci@gmx.de>
2025-03-03 14:30:18 +0100

    ViewModels

---
+++ BlazorUI/Components/Pages/Home.razor.cs
@@ -0,0 +1,8 @@
+using Domain.Entities;
+using Shared.ViewModels;
+
+namespace BlazorUI.Components.Pages;
+public partial class Home
+{
+    public QuoteOfTheDayViewModel? QotdViewModel { get; set; }
+}
