e00a0cd5e8d8ee9386a938a993e4ec8d874a0fdf
Ali Ilci <Ali.Ilci@gmx.de>
2025-03-04 12:05:17 +0100

    PersistComponent

--- BlazorUI/Components/Pages/Home.razor.cs
+++ BlazorUI/Components/Pages/Home.razor.cs
@@ -7,14 +7,39 @@ using Shared.ViewModels;
 
 namespace BlazorUI.Components.Pages;
 
-public partial class Home
+public partial class Home : IDisposable
 {
     [Inject] public IQotdService QotdService { get; set; } = null!;
+    [Inject] public PersistentComponentState ApplicationState { get; set; } = null!;
+    private PersistingComponentStateSubscription _persistingComponentStateSubscription;
 
     public QuoteOfTheDayViewModel? QotdViewModel { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
-        QotdViewModel = await QotdService.GetQuoteOfTheDayAsync();
+        _persistingComponentStateSubscription = ApplicationState.RegisterOnPersisting(PersistData);
+        var statedLoaded = ApplicationState.TryTakeFromJson<QuoteOfTheDayViewModel>("qotd", out var qotd);
+
+        QotdViewModel = statedLoaded && qotd is not null ? qotd : await QotdService.GetQuoteOfTheDayAsync();
+    }
+
+    private Task PersistData()
+    {
+        ApplicationState.PersistAsJson("qotd", QotdViewModel);
+        return Task.CompletedTask;
+    }
+
+    //protected override async Task OnAfterRenderAsync(bool firstRender)
+    //{
+    //    if (firstRender)
+    //    {
+    //        QotdViewModel = await QotdService.GetQuoteOfTheDayAsync();
+    //        StateHasChanged();
+    //    }
+    //}
+
+    public void Dispose()
+    {
+        _persistingComponentStateSubscription.Dispose();
     }
 }
