0a838501a9438405c04f8a5ca7f4f4a34fe61dde
Ali Ilci <Ali.Ilci@gmx.de>
2025-03-04 16:19:28 +0100

    AuthorReloaded

---
+++ BlazorUI/Components/Pages/Author/OverviewReloaded.razor.cs
@@ -0,0 +1,34 @@
+using Logging;
+using Microsoft.AspNetCore.Components;
+using Services;
+using Shared.ViewModels;
+
+namespace BlazorUI.Components.Pages.Author;
+public partial class OverviewReloaded
+{
+    [Inject] public ILoggerManager Logger { get; set; } = null!;
+    [Inject] public IQotdService QotdService { get; set; } = null!;
+    public IEnumerable<AuthorViewModel>? AuthorsVm { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await GetAuthors();
+    }
+
+    private async Task GetAuthors()
+    {
+        AuthorsVm = (await QotdService.GetAuthorsAsync()).OrderBy(c => c.Name);
+    }
+
+    private async Task DeleteAuthor(Guid authorId)
+    {
+        Logger.LogInformation($"Autor mit der Id: {authorId} zum Löschen ausgewählt...");
+
+        var deletedAuthor = await QotdService.DeleteAuthorAsync(authorId);
+
+        if (deletedAuthor is not null)
+        {
+            await GetAuthors();
+        }
+    }
+}
