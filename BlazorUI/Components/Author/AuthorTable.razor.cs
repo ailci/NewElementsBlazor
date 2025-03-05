76ff66a5aab034d63c722af87dbd2c1610cf8566
Ali Ilci <Ali.Ilci@gmx.de>
2025-03-05 11:13:48 +0100

    Confirm

--- BlazorUI/Components/Author/AuthorTable.razor.cs
+++ BlazorUI/Components/Author/AuthorTable.razor.cs
@@ -1,4 +1,5 @@
 using ComponentsLibrary;
+using ComponentsLibrary.Components;
 using Domain.Entities;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
@@ -11,9 +12,13 @@ public partial class AuthorTable
     [Inject] public DialogService DialogService { get; set; } = null!;
     [Parameter] public IEnumerable<AuthorViewModel>? AuthorViewModels { get; set; }
     [Parameter] public EventCallback<Guid> OnAuthorDelete { get; set; }
+    private ConfirmDialog? _confirmDialogComponent; //Referenz zur Componente
+    private Guid _authorIdToDelete;
 
     private async Task ShowConfirmationDialog(AuthorViewModel authorVm)
     {
+        _authorIdToDelete = authorVm.Id;
+
         // 1. Version Klassik
         //if (await JsRuntime.InvokeAsync<bool>("confirm", $"Wollen Sie wirklich den Autor {authorVm.Name} löschen?"))
         //{
@@ -27,5 +32,12 @@ public partial class AuthorTable
         //}
 
         // 3. Version als Componente
+        _confirmDialogComponent?.Show($"Wollen Sie wirklich den Autor {authorVm.Name} löschen?");
+    }
+
+    private async Task ConfirmDeleteClicked()
+    {
+        //TODO: Authorid übergeben
+        await OnAuthorDelete.InvokeAsync(_authorIdToDelete);
     }
 }
--- ComponentsLibrary/Components/ConfirmDialog.razor.cs
+++ ComponentsLibrary/Components/ConfirmDialog.razor.cs
@@ -1,5 +1,20 @@
+using Microsoft.AspNetCore.Components;
+
 namespace ComponentsLibrary.Components;
 public partial class ConfirmDialog
 {
+    [Parameter, EditorRequired] public string ConfirmTitle { get; set; } = string.Empty;
+    [Parameter] public string ConfirmMessage { get; set; } = "Sind Sie sicher?";
+    [Parameter] public EventCallback<bool> OnConfirmClicked { get; set; }
+    private bool _showConfirm;
 
+    public void Show()
+    {
+        _showConfirm = true;
+    }
+    public void Show(string message)
+    {
+        _showConfirm = true;
+        ConfirmMessage = message;
+    }
 }
