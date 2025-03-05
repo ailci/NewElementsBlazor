4c2166fc56982ea871aeddece3373b1a798a0638
Ali Ilci <Ali.Ilci@gmx.de>
2025-03-05 12:39:19 +0100

    Formular

--- BlazorUI/Components/Pages/Author/AuthorNew.razor.cs
+++ BlazorUI/Components/Pages/Author/AuthorNew.razor.cs
@@ -1,5 +1,13 @@
+using Microsoft.AspNetCore.Components.Forms;
+using Shared.ViewModels;
+
 namespace BlazorUI.Components.Pages.Author;
 public partial class AuthorNew
 {
+    public AuthorForCreateViewModel? AuthorForCreateVm { get; set; }
 
+    private Task HandleValidSubmit(EditContext arg)
+    {
+        return Task.CompletedTask;
+    }
 }
---
+++ Shared/ViewModels/AuthorForCreateViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Shared.ViewModels;
+
+public class AuthorForCreateViewModel
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateOnly? BirthDate { get; set; }
+    public IBrowserFile? Photo { get; set; }
+}
